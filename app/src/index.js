6c37af0334d81b7172f12d32a0ae8043e80d1e69
Alberto <alberto@mendezcabrera.com>
2017-10-26 22:35:50 +0200

    Initial commit

---
+++ app/src/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var {buildSchema} = require('graphql');
+
+var Book = [
+    {
+        "id": 1,
+        "title": "El hombre invisible",
+        "price": "10.95E",
+        "author": "Autor 1"
+    },
+    {
+        "id": 2,
+        "title": "un mundo feliz",
+        "price": "20.30E",
+        "author": "Isaac Asimov"
+    },
+];
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`  
+  type Book {
+    id: ID
+    title: String
+    price: String
+    author: Author
+  }
+  
+  type Author {
+    id: ID
+    firstName: String
+    lastName: String
+    books: [Book]
+  }
+  
+  type Query {
+    hello: String
+    book(id: ID!): String
+    author(id: ID!): Author
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hola mundo!';
+    },
+    /*book: (id) => {
+       // var book = Book.entries[parseInt(id)];
+        return "ID seleccionado: ";
+    },*/
+    book: function ({id}) {
+        return "Devolver libro con ID: " + id;
+    },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
